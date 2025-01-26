282fd974bef65121f26eadccc5fab519854b8356
boobaGreen <claudiodallara77@gmail.com>
2025-01-26 23:45:35 +0100

    bounce twitter share button

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -8,18 +8,18 @@ type TModal = {
 export default function Modal({ confirmRemoveUser, cancelRemoveUser }: TModal) {
   // Rendering a modal with confirmation and cancellation buttons
   return (
-    <div className="fixed inset-0 bg-black flex items-center justify-center z-50 shadow-xl opacity-90">
-      <div className="bg-customTurqueseDark p-6 rounded-lg ">
+    <div className="fixed inset-0  flex items-center justify-center z-50 shadow-xl opacity-90">
+      <div className="bg-customTurquese p-6 rounded-lg ">
         <p>Sei sicuro di voler rimuovere l'utente?</p>
         <div className="flex justify-center gap-6 ">
           <button
-            className="bg-customGrey text-ustomTurquese px-4 py-2 rounded mt-4"
+            className="bg-customGrey text-ustomTurquese px-4 py-2 rounded mt-4 text-white"
             onClick={confirmRemoveUser}
           >
             Conferma
           </button>
           <button
-            className="bg-customGrey text-ustomTurquese px-4 py-2 rounded mt-4"
+            className="bg-customGrey text-ustomTurquese px-4 py-2 rounded mt-4 text-white"
             onClick={cancelRemoveUser}
           >
             Annulla
